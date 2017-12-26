7cc5ab0205fe7c0fd8280c10d7766f6dc5bf137e
Snake <tiramsnake@gmail.com>
2017-12-26 21:19:33 +0200

    m2ctrl

---
+++ Ctrl/main.cpp
@@ -0,0 +1,52 @@
+#include <zmq.hpp>
+#include <iostream>
+
+void start()
+{
+   system("nohup ./m2DatabaseService &");
+   system("nohup ./m2InboxService &");
+   system("nohup ./m2ActionsService &");
+   system("nohup ./m2Calendar &");
+   system("nohup ./m2central &");
+   system("nohup ./run_wa.sh &");
+
+   std::cout << "Done\n";
+}
+
+void stop()
+{
+   system("pkill m2DatabaseServi");
+   system("pkill m2InboxService");
+   system("pkill m2ActionsServic");
+   system("pkill m2Calendar");
+   system("pkill m2central");
+   system("pkill WebApp.sh");
+
+   std::cout << "Done\n";
+}
+
+int main(int argc, char *argv[])
+{
+   if(argc != 2)
+   {
+      std::cout << "Please specify command to run";
+      return -1;
+   }
+
+   std::string commandName = argv[1];
+   if(commandName == "start")
+   {
+      start();
+   }
+   else if(commandName == "stop")
+   {
+      stop();
+   }
+   else
+   {
+      std::cout << "Unknown command " << commandName.c_str();
+      return -1;
+   }
+   
+   return 0;
+}
