579249f54e2533af0acc23418a601d23e9604860
snake <tiramsnake@gmail.com>
2020-07-09 08:26:22 +0300

    sdf

--- WebApp2/client/src/components/FinanceView.jsx
+++ WebApp2/client/src/components/FinanceView.jsx
@@ -3,8 +3,52 @@ import MateriaRequest from '../modules/materia_request'
 
 import {
     Typography,
+    Table,
+    TableRow,
+    TableCell,
+    TableHead,
+    TableBody,
     Grid
 } from "@material-ui/core";
+
+function getCell(field, obj)
+{
+    if(field === 'id' || field === 'typename')
+    {
+        return (<div/>);
+    }
+
+    return <TableCell>
+        {obj[field] && obj[field] / 100}
+    </TableCell>
+}
+
+function getCategoryRow(field, value, keys)
+{
+    if(field === 'id' || field === 'status' || field === 'typename' || field === 'balance' || field === 'totalPerMonth')
+    {
+        return (<div/>);
+    }
+
+    return <TableRow>
+        <TableCell>{field}</TableCell>
+        {keys.map((obj) => getCell(obj, value))}
+        <TableCell>{value.total}</TableCell>
+    </TableRow>
+}
+
+function getReportTable(report)
+{
+    return (<Table size="small">
+        <TableHead>
+        {getHeaderRow(report.totalPerMonth)}
+        </TableHead>
+        <TableBody>
+            {Object.keys(report).map((obj) => getCategoryRow(obj, report[obj], Object.keys(report.totalPerMonth)))}
+            {getCategoryRow("Total", report.totalPerMonth, Object.keys(report.totalPerMonth))}
+        </TableBody>
+    </Table>);
+}
 
 function FinanceView(props) 
 {
@@ -23,13 +67,13 @@ function FinanceView(props)
     }
 
     return (
-        <div>{report && 
+        <div>{report && ( 
             <Grid  style={{paddingTop:'5px'}} container direction="column" justify="center" alignItems="center">
                 <Typography variant="h6" color={report.status === "Critical" ? "red" : "primary"}>
                     Status: {report.status}, Balance: {report.balance / 100}€
                 </Typography>
-            </Grid>
-            }
+            </Grid>)}
+            {report && getReportTable(report)}
         </div>
     );
 }
