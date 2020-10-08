50a0286cef0fa111682720e802ea7a7ec5715bad
snake <tiramsnake@gmail.com>
2020-10-09 01:18:14 +0200

    fix

--- WebApp2/client/src/components/StrategyView.jsx
+++ WebApp2/client/src/components/StrategyView.jsx
@@ -287,7 +287,7 @@ function StrategyView(props)
     const [selectNodeActive, setSelectNodeActive] = useState(false);
     const [deleteTarget, setDeleteTarget] = useState(null);
     const [showCompleted, setShowCompleted] = useState(true);
-    const [showLocked, setShowLocked] = useState(false);
+    const [showLocked, setShowLocked] = useState(true);
 
     if(graphData == null)
     {
