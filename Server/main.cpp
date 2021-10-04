b64f80829bef1eb038c8b8935ba042b9ade134a2
alexander.rodichkin <alexander.rodichkin@navinfo.eu>
2021-10-04 20:05:26 +0200

    Server proper shutdown

--- Server/main.cpp
+++ Server/main.cpp
@@ -57,6 +57,7 @@ private:
 DoubleLogger logger("m3server.log");
 std::mutex gMainMutex;
 static bool shutdownFlag = false;
+served::net::server* server = nullptr; 
 
 void timerFunc(materia::ICore3* core)
 {
@@ -66,6 +67,7 @@ void timerFunc(materia::ICore3* core)
     {
         if(shutdownFlag)
         {
+            server->stop();
             return;
         }
 
@@ -104,11 +106,10 @@ void newFunc2(std::string password, materia::ICore3* core)
     try
     {
         Codec codec(password);
-        served::net::server* server = nullptr; 
         served::multiplexer mux;
 
         mux.handle("/api")
-            .get([&codec, core, server](served::response & res, const served::request & req) {
+            .post([&codec, core, server](served::response & res, const served::request & req) {
                 std::string decoded;
 
                 std::string toSend = "";
@@ -141,25 +142,23 @@ void newFunc2(std::string password, materia::ICore3* core)
 
                 toSend = base64_encode(codec.encrypt(toSend));
 
-                if(shutdownFlag)
-                {
-                    server->stop();
-                }
-
                 res << toSend;    
+
 		});
 
-        served::net::server srv("127.0.0.1", "5755", mux);
+        served::net::server srv("127.0.0.1", "5754", mux);
         server = &srv;
         srv.run(4);
     }
     catch(std::exception& e)
     {
         logger << "Server has crashed with an error: " << e.what();
+        abort();
     }
     catch(...)
     {
         logger << "Server has crashed for unknown reason";
+        abort();
     }
 }
 
