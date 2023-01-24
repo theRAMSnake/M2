3facfa0d2da015a1341b7e8032200d83ea0bd671
alexander.rodichkin <alexander.rodichkin@navinfo.eu>
2023-01-24 13:16:24 +0100

    icron

---
+++ cron/backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+touch 2.txt
+
+./m4tools "{\"operation\":\"backup\", \"params\":{}}"
+sshpass -p "hailtothekingbaby" scp -r snake@ramsnake.net:/materia/materia.back ./materia.bu$(date +%s)
+
+#news
+java -jar NewsMaker.jar
+
+#invest
+./M2/surveys/invest.py 2RHVKGRYT9UL12E5
+./M2/analyzers/invest.py
