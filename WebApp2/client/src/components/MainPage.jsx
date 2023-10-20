548152e0ceb21d973b5ddbfad220b98ce9651ac3
Rodichkin <arodich@88665a4ba4d6.ant.amazon.com>
2023-10-20 19:10:12 +0200

    ver

--- WebApp2/client/src/components/MainPage.jsx
+++ WebApp2/client/src/components/MainPage.jsx
@@ -268,7 +268,7 @@ function MainPage(props) {
                         <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" className={classes.title}>
-                        Materia
+                        Materia v 3.7.1
                     </Typography>
                     <ContractsCtrl/>
                     <SearchBar onSubmit={searchBarSubmit}/>
