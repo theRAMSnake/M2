1fff996bba695b269c4ea823ed3e9bf334c3eb2f
Rodichkin <arodich@88665a4ba4d6.ant.amazon.com>
2023-10-23 10:15:59 +0200

    yli -10

--- Core/library/daily.py
+++ Core/library/daily.py
@@ -2,8 +2,14 @@ from datetime import datetime
 import collection
 import m4
 import ambitions
+import reward
 
 def m4_update(today):
+    current_day = datetime.now().weekday()
+
+    # Check if the current day is a weekday
+    if 0 <= current_day <= 4:  # 0 is Monday, 4 is Friday
+        reward.reward(-10)
     ambitions.update(today)
 
 def daily_update():
