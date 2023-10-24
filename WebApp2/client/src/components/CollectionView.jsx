b03f8e2d6209da95556d6ae753c2f1dcb8bc71bc
Rodichkin <arodich@88665a4ba4d6.ant.amazon.com>
2023-10-24 21:05:18 +0200

    style

--- WebApp2/client/src/components/CollectionView.jsx
+++ WebApp2/client/src/components/CollectionView.jsx
@@ -6,6 +6,7 @@ import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
 import Materia from '../modules/materia_request'
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
