71007891c5c2f88af28b171368972b7d07304702
Rodichkin <arodich@88665a4ba4d6.ant.amazon.com>
2023-10-24 21:07:57 +0200

    style

--- WebApp2/client/src/components/CollectionView.jsx
+++ WebApp2/client/src/components/CollectionView.jsx
@@ -7,6 +7,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
 import Materia from '../modules/materia_request'
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
--- WebApp2/client/src/components/MainPage.jsx
+++ WebApp2/client/src/components/MainPage.jsx
@@ -278,7 +278,7 @@ function MainPage(props) {
                         <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" className={classes.title}>
-                        Materia v3.7.14
+                        Materia v3.7.15
                     </Typography>
                     <ContractsCtrl/>
                     <SearchBar onSubmit={searchBarSubmit}/>
