eebc8cb3be1578f1175c01ccda4687d60f3a215d
Rodichkin <arodich@88665a4ba4d6.ant.amazon.com>
2023-12-01 16:16:18 +0100

    iiiii

--- WebApp2/client/src/components/DesignerItem.jsx
+++ WebApp2/client/src/components/DesignerItem.jsx
@@ -7,24 +7,20 @@ const DesignerItem = () => {
     const [height, setHeight] = useState(200);
 
     return (
-    <Draggable
-      bounds="parent"
-    >
+    //<Draggable
+    //  bounds="parent"
+    //>
       <ResizableBox width={width} height={height} minConstraints={[100, 100]} maxConstraints={[300, 300]}
         onResizeStop={ ( event, direction, elt, delta ) => {
                 setWidth(width + delta.width);
                 setHeight(height + delta.height);
-                toggleSelection( true );
-            } }
-            onResizeStart={ () => {
-                toggleSelection( false );
             } }
         >
         <div style={{ border: '1px solid white', height: '100%', width: '100%' }}>
             Contents
         </div>
       </ResizableBox>
-    </Draggable>
+    //</Draggable>
   );
 };
 
--- WebApp2/client/src/components/MainPage.jsx
+++ WebApp2/client/src/components/MainPage.jsx
@@ -299,7 +299,7 @@ function MainPage(props) {
                         <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" className={classes.title}>
-                        Materia v3.7.99
+                        Materia v3.7.100
                     </Typography>
                     <ContractsCtrl/>
                     <SearchBar onSubmit={searchBarSubmit}/>
