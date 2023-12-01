416c761d778d63d3b51182f19d170d63a20148c0
Rodichkin <arodich@88665a4ba4d6.ant.amazon.com>
2023-12-01 16:04:43 +0100

    iiii

--- WebApp2/client/src/components/DesignerItem.jsx
+++ WebApp2/client/src/components/DesignerItem.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Draggable from 'react-draggable';
 import { ResizableBox } from 'react-resizable';
 
 const DesignerItem = () => {
+    const [width, setWidth] = useState(200);
+    const [height, setHeight] = useState(200);
+
     return (
     <Draggable
       bounds="parent"
     >
-      <ResizableBox width={200} height={200} minConstraints={[100, 100]} maxConstraints={[300, 300]}>
+      <ResizableBox width={width} height={height} minConstraints={[100, 100]} maxConstraints={[300, 300]}
+        onResizeStop={ ( event, direction, elt, delta ) => {
+                setWidth(width + delta.width);
+                setHeight(height + delta.height);
+                toggleSelection( true );
+            } }
+            onResizeStart={ () => {
+                toggleSelection( false );
+            } }
+        >
         <div style={{ border: '1px solid white', height: '100%', width: '100%' }}>
             Contents
         </div>
--- WebApp2/client/src/components/MainPage.jsx
+++ WebApp2/client/src/components/MainPage.jsx
@@ -299,7 +299,7 @@ function MainPage(props) {
                         <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" className={classes.title}>
-                        Materia v3.7.97
+                        Materia v3.7.99
                     </Typography>
                     <ContractsCtrl/>
                     <SearchBar onSubmit={searchBarSubmit}/>
