e9732c824c4dd209baac44ccc0ce80b8eab7470a
Rodichkin <arodich@88665a4ba4d6.ant.amazon.com>
2023-12-01 16:23:56 +0100

    iiiii

--- WebApp2/client/src/components/DesignerItem.jsx
+++ WebApp2/client/src/components/DesignerItem.jsx
@@ -1,16 +1,16 @@
 import React, {useState} from 'react';
 import Draggable from 'react-draggable';
-import { ResizableBox } from 'react-resizable';
+import { Resizable } from 're-resizable';
 
 const DesignerItem = () => {
     const [width, setWidth] = useState(200);
     const [height, setHeight] = useState(200);
 
     return (
-    //<Draggable
-    //  bounds="parent"
-    //>
-      <ResizableBox width={width} height={height} minConstraints={[100, 100]} maxConstraints={[300, 300]}
+    <Draggable
+      bounds="parent"
+    >
+      <Resizable size={{width: width, height: height}}
         onResizeStop={ ( event, direction, elt, delta ) => {
                 setWidth(width + delta.width);
                 setHeight(height + delta.height);
@@ -19,8 +19,8 @@ const DesignerItem = () => {
         <div style={{ border: '1px solid white', height: '100%', width: '100%' }}>
             Contents
         </div>
-      </ResizableBox>
-    //</Draggable>
+      </Resizable>
+    </Draggable>
   );
 };
 
