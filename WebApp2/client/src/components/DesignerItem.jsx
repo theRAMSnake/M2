28cb513b6426dc6853d36b6ea8be85d5af849735
Rodichkin <arodich@88665a4ba4d6.ant.amazon.com>
2023-12-01 18:02:45 +0100

    iii

--- WebApp2/client/src/components/DesignerItem.jsx
+++ WebApp2/client/src/components/DesignerItem.jsx
@@ -2,13 +2,15 @@ import React, {useState} from 'react';
 import Draggable from 'react-draggable';
 import { Resizable } from 're-resizable';
 
+
 const DesignerItem = () => {
     const [width, setWidth] = useState(200);
     const [height, setHeight] = useState(200);
 
-    return (
+return (
     <Draggable
       bounds="parent"
+      handle=".drag-handle"
     >
       <Resizable size={{width: width, height: height}}
         onResizeStop={ ( event, direction, elt, delta ) => {
@@ -19,10 +21,10 @@ const DesignerItem = () => {
         <div className="drag-handle" style={{ cursor: 'move', backgroundColor: 'lightblue', userSelect: 'none' }}>
           Drag here
         </div>
+        <div style={{ border: '1px solid white', height: '100%', width: '100%' }}>
+            Contents
+        </div>
       </Resizable>
-      <div style={{ border: '1px solid white', height: '100%', width: '100%' }}>
-          Contents
-      </div>
     </Draggable>
   );
 };
