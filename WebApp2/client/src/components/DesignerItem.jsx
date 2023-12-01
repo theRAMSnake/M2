631d3f5e3970b93d8a99fcbee59b49b08aa37080
Rodichkin <arodich@88665a4ba4d6.ant.amazon.com>
2023-12-01 16:38:56 +0100

    iiii

--- WebApp2/client/src/components/DesignerItem.jsx
+++ WebApp2/client/src/components/DesignerItem.jsx
@@ -19,10 +19,10 @@ const DesignerItem = () => {
         <div className="drag-handle" style={{ cursor: 'move', backgroundColor: 'lightblue', userSelect: 'none' }}>
           Drag here
         </div>
-        <div style={{ border: '1px solid white', height: '100%', width: '100%' }}>
-            Contents
-        </div>
       </Resizable>
+      <div style={{ border: '1px solid white', height: '100%', width: '100%' }}>
+          Contents
+      </div>
     </Draggable>
   );
 };
--- WebApp2/client/src/components/MainPage.jsx
+++ WebApp2/client/src/components/MainPage.jsx
@@ -299,7 +299,7 @@ function MainPage(props) {
                         <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" className={classes.title}>
-                        Materia v3.7.103
+                        Materia v3.7.104
                     </Typography>
                     <ContractsCtrl/>
                     <SearchBar onSubmit={searchBarSubmit}/>
