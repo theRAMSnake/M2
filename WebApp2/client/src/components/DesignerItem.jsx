36c25ac0cebbc01cc3dbca659a3898a5d91fad5e
Rodichkin <arodich@88665a4ba4d6.ant.amazon.com>
2023-12-01 16:32:24 +0100

    iiiii

--- WebApp2/client/src/components/DesignerItem.jsx
+++ WebApp2/client/src/components/DesignerItem.jsx
@@ -16,6 +16,9 @@ const DesignerItem = () => {
                 setHeight(height + delta.height);
             } }
         >
+        <div className="drag-handle" style={{ cursor: 'move', backgroundColor: 'lightblue', userSelect: 'none' }}>
+          Drag here
+        </div>
         <div style={{ border: '1px solid white', height: '100%', width: '100%' }}>
             Contents
         </div>
