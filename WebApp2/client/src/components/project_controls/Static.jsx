6a107b7cab85f5a2b6726fde32ee37c548a3e00b
Rodichkin <arodich@88665a4ba4d6.ant.amazon.com>
2023-12-17 11:08:11 +0100

    iiii

--- WebApp2/client/src/components/project_controls/Static.jsx
+++ WebApp2/client/src/components/project_controls/Static.jsx
@@ -3,7 +3,7 @@ import RichEditor from '../RichEditor.jsx'
 
 const Static = ({ control }) => {
     return (
-        <div dangerouslySetInnerHTML={control.value} </div>
+        <div dangerouslySetInnerHTML={control.value}/>
       );
 };
 
