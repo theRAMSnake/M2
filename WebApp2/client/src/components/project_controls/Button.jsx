23c8cdc7e8fc7aad3e458d6ac9e6d6f44abfddfd
Rodichkin <arodich@88665a4ba4d6.ant.amazon.com>
2023-12-25 16:52:22 +0100

    iiiii

--- WebApp2/client/src/components/project_controls/Button.jsx
+++ WebApp2/client/src/components/project_controls/Button.jsx
@@ -19,36 +19,45 @@ export class PushButtonEditor extends React.Component {
     constructor(props) {
         super(props);
         const { control } = this.props;
-        this.myObject = {...control};
+        this.state = {
+            myObject: {...control}
+        }
     }
 
     getContent() {
-        const { control } = this.props;
-        return this.myObject;
+        const { myObject } = this.state;
+        return myObject;
     }
 
     handleCaptionChange = (e) => {
-        this.myObject = {...this.myObject, caption: e.target.value};
+        this.setState(prevState => ({
+            myObject : {...prevState.myObject, caption: e.target.value};
+        }));
     }
 
     handleConfirmationChange = (e) => {
-        this.myObject = {...this.myObject, need_confirmation: String(e.target.checked)};
+        this.setState(prevState => ({
+            myObject : {...prevState.myObject, need_confirmation: e.target.checked};
+        }));
     }
 
     handleUpdateChange = (e) => {
-        this.myObject = {...this.myObject, need_update: String(e.target.checked)};
+        this.setState(prevState => ({
+            myObject : {...prevState.myObject, need_update: e.target.checked};
+        }));
     }
 
     render() {
+        const { myObject } = this.state;
         return(
             <div>
-                <TextField inputProps={{onChange: this.handleCaptionChange}} value={this.myObject.value} fullWidth label="Caption" />;
-                <FormControlLabel margin='dense' fullWidth control={<Checkbox inputProps={{onChange: this.handleConfirmationChange}} checked={this.myObject.need_confirmation === "true"} />} label="Need confirmation" />
-                <FormControlLabel margin='dense' fullWidth control={<Checkbox inputProps={{onChange: this.handleUpdateChange}} checked={this.myObject.need_update === "true"} />} label="Need update" />
+                <TextField inputProps={{onChange: this.handleCaptionChange}} value={myObject.value} fullWidth label="Caption" />
+                <FormControlLabel margin='dense' fullWidth control={<Checkbox inputProps={{onChange: this.handleConfirmationChange}} checked={myObject.need_confirmation === "true"} />} label="Need confirmation" />
+                <FormControlLabel margin='dense' fullWidth control={<Checkbox inputProps={{onChange: this.handleUpdateChange}} checked={myObject.need_update === "true"} />} label="Need update" />
                 <AceEditor
                   mode="python"
                   theme="monokai" // Choose your preferred theme
-                  onChange={(newValue) => {this.myObject.script = newValue;}}
+                  onChange={(newValue) => {myObject.script = newValue;}}
                   name="json-editor"
                   editorProps={{ $blockScrolling: true }}
                   value={this.myObject.script}
